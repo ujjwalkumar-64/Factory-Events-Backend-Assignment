b2cdaef8fc4fb4fc2f8c0550401004c30fc24a4d
ujjwalkumar-64 <ujjwalkumar6281@gmail.com>
2026-01-16 22:37:54 +0530

    swagger integrated for api doc

---
+++ src/main/java/com/assignment/buyogo_backend_assignment/config/OpenApiConfig.java
@@ -0,0 +1,21 @@
+package com.assignment.buyogo_backend_assignment.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Factory Event Bulk Process API")
+                        .version("1.0.0")
+                        .description("API documentation for the Factory Event Process Application"))
+                .addServersItem(new Server().url("http://localhost:8092").description("Local server"));
+    }
+}
