38a44ad44af1b74bbfe62aadbc8657c96cc09845
ujjwalkumar-64 <ujjwalkumar6281@gmail.com>
2026-01-16 12:57:55 +0530

    Project initialized

---
+++ src/main/java/com/assignment/buyogo_backend_assignment/BuyogoBackendAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.assignment.buyogo_backend_assignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuyogoBackendAssignmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BuyogoBackendAssignmentApplication.class, args);
+    }
+
+}
